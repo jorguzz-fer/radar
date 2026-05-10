76077f218c5e66d89d67b9ae93d5af0da217763b
jorguzz-fer <fer.jorge@gmail.com>
2026-05-10 15:50:05 -0300

    fix: webpack externals customizado sem nodeExternals + resolve.symlinks=false para pnpm

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,12 +1,21 @@
-const nodeExternals = require('webpack-node-externals')
-
 module.exports = (options) => {
   return {
     ...options,
+    resolve: {
+      ...options.resolve,
+      symlinks: false,
+    },
     externals: [
-      nodeExternals({
-        allowlist: [/^@radar\//],
-      }),
+      ({ request }, callback) => {
+        if (!request) return callback()
+        // Bundle workspace packages inline
+        if (request.startsWith('@radar/')) return callback()
+        // Externalize all other bare module specifiers
+        if (!request.startsWith('.') && !request.startsWith('/')) {
+          return callback(null, 'commonjs ' + request)
+        }
+        callback()
+      },
     ],
   }
 }
